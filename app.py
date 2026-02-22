d862ec0eca4248d77aefbd6e7e0d4b5dd8932445
hasanmahmudcst-pixel <hasanmahmud.cst@gmail.com>
2026-02-23 01:34:26 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
-import os
 
+# template_folder='.' কারণ আপনার সব HTML ফাইল মেইন ডিরেক্টরিতে আছে
 app = Flask(__name__, template_folder='.')
 
+# ১. হোম পেজ (Index)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# ২. লগইন পেজ (Admin Login)
+@app.route('/login')
+def login():
+    return render_template('admin_login.html')
+
+# ৩. অথেনটিকেশন পেজ (Auth)
+@app.route('/auth')
+def auth():
+    return render_template('auth.html')
+
+# ৪. অ্যাডমিন প্যানেল (Admin)
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+# ৫. আইটেম যোগ করার পেজ (Add Item)
+@app.route('/add')
+def add_item():
+    return render_template('add_item.html')
 
 if __name__ == '__main__':
     app.run()
